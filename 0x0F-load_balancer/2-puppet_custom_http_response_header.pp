39bc1520078cc0cdbb092fe8de24b45deca67c8a
Pibo Shongwe <piboman@yahoo.com>
2024-03-04 19:46:45 -0800

    u

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,25 @@
 # install nginx and add custom header
 
 exec { 'update':
+  provider => shell,
   command => '/usr/bin/apt-get -y update',
   before  => Exec['install Nginx'],
 }
 
 exec { 'install Nginx':
+  provider => shell,
   command => '/usr/bin/apt-get -y install nginx',
   before  => Exec['add_header'],
 }
 
 exec { 'add_header':
+  provider => shell,
   command     => "/bin/sed -i 's/include \\/etc\\/nginx\\/sites-enabled\\/*;/include \\/etc\\/nginx\\/sites-enabled\\/*;\\n\\tadd_header X-Served-By \"\$HOSTNAME\";/' /etc/nginx/nginx.conf",
   environment => ['HOSTNAME=${hostname}'],
   before      => Exec['restart Nginx'],
 }
 
 exec { 'restart Nginx':
+  provider => shell,
   command => '/usr/sbin/service nginx restart',
 }
