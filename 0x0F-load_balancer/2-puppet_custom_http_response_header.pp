f8ca44f91b3d4abcd8b3030d49e45f6b414d3a4a
Pibo Shongwe <piboman@yahoo.com>
2024-03-04 19:50:09 -0800

    u

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -14,8 +14,8 @@ exec { 'install Nginx':
 
 exec { 'add_header':
   provider => shell,
-  command     => "/bin/sed -i 's/include \\/etc\\/nginx\\/sites-enabled\\/*;/include \\/etc\\/nginx\\/sites-enabled\\/*;\\n\\tadd_header X-Served-By \"\$HOSTNAME\";/' /etc/nginx/nginx.conf",
-  environment => ['HOSTNAME=${hostname}'],
+  command     => "/bin/sed -i 's/include \\/etc\\/nginx\\/sites-enabled\\/*;/include \\/etc\\/nginx\\/sites-enabled\\/*;\\n\\tadd_header X-Served-By \"\$HN\";/' /etc/nginx/nginx.conf",
+  environment => ['HN=${hostname}'],
   before      => Exec['restart Nginx'],
 }
 
