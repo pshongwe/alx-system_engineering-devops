41fb987563eff4bca0e70cccddcd07fc9018cc4d
Pibo Shongwe <piboman@yahoo.com>
2024-03-25 20:32:18 -0700

    u

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,22 +5,11 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos"
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    try:
-        user = requests.get(user_url).json()
-        todos = requests.get(todos_url, params={"userId": employee_id}).json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
-        return
-
-    completed_tasks = [todo for todo in todos if todo.get("completed")]
-    a = user.get('name')
-    b = len(completed_tasks)
-    c = len(todos)
-    print(f"Employee {a} is done with tasks({b}/{c}): ")
-    for task in completed_tasks:
-        print(f"\t {task.get('title')}")
+    completed = [x.get("title") for x in todos if x.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(s)) for s in completed]
